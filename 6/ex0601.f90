6ba8da1318b5b105d4088bc16d970a78faed74da
LinStar <31849610+Monotone1997@users.noreply.github.com>
2018-05-10 14:40:37 +0800

    Create ex0601.f90

---
+++ 6/ex0601.f90
@@ -0,0 +1,27 @@
+SUBROUTINE init_random_seed()
+INTEGER :: i, n, clock
+INTEGER, ALLOCATABLE :: seed(:)
+CALL RANDOM_SEED(size = n)
+ALLOCATE(seed(n))
+CALL SYSTEM_CLOCK(COUNT=clock)
+seed = clock + 37 * (/ (i - 1, i = 1, n) /)
+CALL RANDOM_SEED(put = seed)
+DEALLOCATE(seed)
+END SUBROUTINE init_random_seed
+
+PROGRAM EX0601
+REAL::a(2)
+REAL::pi=0
+INTEGER::sum,N=0
+WRITE(*,*)'Please Enter N'
+READ(*,*)N
+CALL init_random_seed()
+DO i=1,N
+   CALL RANDOM_NUMBER(a)
+   IF(a(1)**2+a(2)**2<1) THEN
+     sum=sum+1
+   END IF
+END DO
+pi=4*REAL(sum)/REAL(N)
+WRITE(*,'(A2,A1,F9.7)') 'pi','=',pi
+END
