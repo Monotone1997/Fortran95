7fc35838ce16936eb7baa7c2f06aca94f153ab8b
LinStar <31849610+Monotone1997@users.noreply.github.com>
2018-06-08 13:54:29 +0800

    Update ex1001.f90

--- 10/ex1001.f90
+++ 10/ex1001.f90
@@ -1,27 +1,24 @@
-PROGRAM EX1001
-INTEGER,PARAMETER::n=4
-REAL(KIND=8)::pi=3.141592653589793
-REAL(KIND=8)::x(n)=(/30,45,60,90/)
-REAL(KIND=8)::y(n)=(/sqrt(3.0)/2,sqrt(2.0)/2,1.0/2,0.0/)
-REAL(KIND=8)::temp(n)
-REAL(KIND=8)::t,ty,ty2
-DO i=1,n
-    x(i)=x(i)*pi/180
-END DO
-WRITE(*,*) 'enter the angle'
-READ(*,*) t
-t=t*pi/180
-DO i=1,n
-    temp(i)=y(i)
-    DO j=1,n
-        IF(j==i) CYCLE
-        temp(i)=temp(i)*(t-x(j))/(x(i)-x(j))
-    END DO
-END DO
-ty=sum(temp)
-ty2=cos(t)
-WRITE(*,*) 'result sovled by lagrange interpolation method:'
-WRITE(*,*) ty
-WRITE(*,*) 'result sovled by built-in function:'
-WRITE(*,*) ty2
-END
+program ex1001
+implicit none
+integer::n=4,i,j,k
+real(kind=8)::x(4),y(4),temp(4)
+real(kind=8)::t(3),ty
+x=(/30,45,60,90/)
+y=(/0.866025404,0.707106781,0.5,0.0/)
+t=(/47.0,53.0,79.0/)
+write(*,*) "     degree  ","Lagrange                  ","built-in function" 
+write(*,*) "----------------------------------------------------------"
+do k=1,3
+    ty=0.0
+    do i=1,n
+        temp(i)=y(i)
+        do j=1,n
+            if(j==i) cycle
+            temp(i)=temp(i)*(t(k)-x(j))/(x(i)-x(j))
+        end do
+        ty=ty+temp(i)
+    end do
+    write(*,*) floor(t(k)),ty,cos(t(k)*3.1415926/180)
+end do
+
+end program ex1001
